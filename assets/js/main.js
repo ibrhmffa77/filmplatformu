95d770dae269d5e734767380d0b5d439d4febba1
Reqsane <rqsanibrahimli@gmail.com>
2023-03-14 14:28:06 +0400

    main.css index.html details.html assets new file created added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,31 @@
-var bodyQuery=document.querySelector("body")
-fetch("https://api.tvmaze.com/show")
-.then(response => response.api())
+var row=document.querySelector('.row')
+//$.ajax({
+ //method:(GET),
+ // url: "https://api.tvmaze.com/show"
+//})
+
+fetch('https://api.tvmaze.com/show')
+.then(response => response.json())
   .then(data=>{
-    data.array.forEach(element => {
-      bodyQuery.innerHTML+=`
-      <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">${element.img}
+    data.forEach(element => {
+      row.innerHTML+=`
+      <div class="col-3 py-3">
+      <div class="card " style="width: 18rem;">
+  
   <div class="card-body">
+  <img src="${element.image.medium}" class="card-img-top" alt="...">
     <h5 class="card-title">${element.name}</h5>
-    <p class="card-text"> ${element.country}</p>
     <p class="card-text"> ${element.language}</p>
     <p class="card-text"> ${element.genres}</p>
-    <a href="#" class="btn btn-primary">${element.website}</a>
+    <p class="card-text"> ${element.externals.imdb}</p>
+    <a href="details.html?id=${element.id}" class="btn btn-primary"> click to watch </a>
   </div>
-</div>
-      `
+  </div>
+</div>`
+      
       
     });
 
   })
+
+  
